d819c44f249911d4a4f8c59e3f6587ea966dbce8
keneankit01 <keneankit01@gmail.com>
2021-11-12 04:43:46 +0000

    drivers: Add power driver modifications
    Signed-off-by: keneankit01 <keneankit01@gmail.com>

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#0 SMP PREEMPT Fri Nov 12 16:06:55 EET 2021"
+#define LINUX_COMPILE_BY "emufan"
+#define LINUX_COMPILE_HOST "pop-os"
+#define LINUX_COMPILER "Proton clang version 13.0.0 (https://github.com/llvm/llvm-project b4fd512c36ca344a3ff69350219e8b0a67e9472a) Found CUDA installation: /usr/lib/cuda, version 11.2"
